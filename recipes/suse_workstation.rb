754728a4156c44c8eac6ab82df9d73d4dc566363
autarch princeps <autarch@outlook.com>
2017-09-08 07:34:07 +0200

    -

--- recipes/suse_workstation.rb
+++ recipes/suse_workstation.rb
@@ -5,9 +5,21 @@ include_recipe 'zypper'
 #  uri 'http://pkg.jenkins-ci.org/opensuse/'
 #end
 
-default['vim']['install_method'] = 'package'
-# default['vim']['extra_packages'] = ?
+zypper_package %w[make gcc llvm-clang htop opera zsh powerline powerline-fonts tmux-powerline ruby ruby-devel] do
+	options '--download-in-heaps'
+end
+
+rpm_package 'chefdk' do
+	source 'https://packages.chef.io/files/stable/chefdk/2.2.1/sles/12/chefdk-2.2.1-1.sles12.x86_64.rpm'
+	not_if { ::File.exist?('/opt/chefdk/bin/chef') }
+end
+
+node.default['vim']['install_method'] = 'package'
+node.default['vim']['extra_packages'] = %w[vim-plugin-powerline]
 include_recipe 'vim'
 
-default['chef_dk']['global_shell_init'] = true
-include_recipe 'chef_dk'
+include_recipe 'vagrant'
+
+%w[vagrant-omnibus vagrant-lxc vagrant-aws vagrant-berkshelf vagrant-cachier vagrant-libvirt vagrant-scaleway vagrant-bundler vagrant-git].each do |vp|
+	vagrant_plugin vp
+end
